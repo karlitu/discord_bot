23b1152d2a39729254200b9be6f406ce7ed14914
Karlitu <giacomo.amgm@gmail.com>
2023-06-08 23:02:52 +0200

    add list of members

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import time
 from discord.ext import commands
 from discord import app_commands
 from discord.ext.commands import bot
+import funz as f
 
 
 bot = commands.Bot(command_prefix="/", intents=discord.Intents.all(), description="")
@@ -23,17 +24,34 @@ class abot(discord.Client):
 bot = abot()
 tree = app_commands.CommandTree(bot)
 
-
 @bot.event
 async def on_voice_state_update(member, before, after):
     channel = bot.get_channel(1087402432079863888)
     if channel and len(channel.members) >=1:
         print("test entry")
+        f.save(f.find_name(channel.members), "variables.json")
         if bot.voice_clients:
             return
         await channel.connect()
         print('Bot joined')
 
 
+
+@tree.command(
+  name="people",
+  description="vedi persone in canale",
+  guild=discord.Object(id=1037786437606707221))
+async def self(interaction: discord.Interaction, test: str):
+    channel = bot.get_channel(1087402432079863888)
+    print(f.find_name(channel.members))
+
+    ok = discord.Embed(
+      title=f"test",
+      description=
+      f"fatto",
+      colour=0xff00ff)
+    await interaction.response.send_message(embed=ok)
+    
+    
 tok = "MTExMzg2MTE3MjQ1ODk1ODkzOQ.GRo5J8.hFbfq6rqk-iOL-5_edYIQPOY4T88B6L1IvvSTY"
 bot.run(tok)
--- funz.py
+++ funz.py
@@ -1,1 +1,13 @@
+import json
 
+
+def find_name(member_list):
+    names = []
+    for member in member_list:
+        names.append(member.name)
+    return names
+
+    
+def save(data, filename):
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file)
