e434dfc01b2feadbc01933e13ede57dcdc02ee53
Karlitu <giacomo.amgm@gmail.com>
2023-06-08 22:15:55 +0200

    add bot join to vocal channel

--- bot.py
+++ bot.py
@@ -5,9 +5,7 @@ from discord import app_commands
 from discord.ext.commands import bot
 
 
-bot = commands.Bot(command_prefix="/",
-                   intents=discord.Intents.all(),
-                   description="")
+bot = commands.Bot(command_prefix="/", intents=discord.Intents.all(), description="")
 
 class abot(discord.Client):
 
@@ -18,9 +16,7 @@ class abot(discord.Client):
   async def on_ready(self):
     await tree.sync(guild=discord.Object(id=1037786437606707221))
     self.synced = True
-    print(
-      f"bot{bot.user.name} startato: {time.strftime('%a %b %d %H:%M:%S %Y')}")
-
+    print(f"bot{bot.user.name} startato: {time.strftime('%a %b %d %H:%M:%S %Y')}")
 
 
 
@@ -28,5 +24,16 @@ bot = abot()
 tree = app_commands.CommandTree(bot)
 
 
-tok = "MTA0MjA5NzgxODQ2NDE2NTk1OA.GDk6ZM.OoRYTpX7-Mn4F8XBEfcVsShBspfOtzPdxxPi4A"
+@bot.event
+async def on_voice_state_update(member, before, after):
+    channel = bot.get_channel(1087402432079863888)
+    if channel and len(channel.members) >=1:
+        print("test entry")
+        if bot.voice_clients:
+            return
+        await channel.connect()
+        print('Bot joined')
+
+
+tok = "MTExMzg2MTE3MjQ1ODk1ODkzOQ.GRo5J8.hFbfq6rqk-iOL-5_edYIQPOY4T88B6L1IvvSTY"
 bot.run(tok)
