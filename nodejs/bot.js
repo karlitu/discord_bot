5bcf0488f86f3a70b3d3e2dbface2e835223e7e3
Marta710 <118460179+Marta710@users.noreply.github.com>
2023-07-31 20:38:51 +0200

    finito
    così è più ordinato

--- nodejs/bot.js
+++ nodejs/bot.js
@@ -1,32 +1,49 @@
-//In bot.js
-const token = "MTEzMzExNjA3MDE5MDkwNzU0Mw.GZeMFt.yyrFPM-BcghLiLE2SvdWBV1MSvdU8rfjVyy7Zo" //Token that you saved in step 5 of this tutorial
-const {Client} = require("discord.js");
-
+const { Client } = require("discord.js");
+const f = require("./funz.js");
 const client = new Client({
     intents:[
-        "GUILDS",
-        "GUILD_MESSAGES",
-        "DIRECT_MESSAGES"
+        "Guilds",
+        "GuildMessages",
+        "DirectMessages",
+        "GuildVoiceStates",
+        "MessageContent",
     ]
 });
+const { token } = require('C:\\Users\\user\\OneDrive\\Documenti\\jsbot-main\\config.json');
+const {
+    prefix
+} = require('./data.json')
 
-client.on("ready", () =>{
-  console.log(client.user.username + "is online");
-  client.on('messageCreate', (msg) => {
-    switch (msg.content) {
-      case "ping":
-        msg.channel.send("pong")
-        break;
-      case "pong":
-        msg.channel.send("pang")
-        break;
-      default:
-        break;
-    }
-  });
-});
 client.login(token)
-
-
-
-//  https://discordjs.guide/
+client.on("ready", () => {
+    console.log("online")
+    client.on("messageCreate", async msg => {
+        let message = msg.content;
+        if (message.startsWith(prefix)){
+            command = f.split_arg(message)
+            console.log(command);
+            switch (command[0]) {
+                case "join":
+                    connection = f.joins(msg);
+                    connection;
+                    break;
+                case "exit":
+                    console.log("exit");
+                    f.exits(msg, connection);
+                    break;
+                    case "play":
+                        connection = f.joins(msg);
+                        try {
+                            connection;
+                        } catch (error) {
+                            console.log("unable to connect")
+                        };
+                        f.songInfo(command[1], msg);
+                        f.playSong(command[1]);
+                        break;
+                default:
+                    break;
+            }
+        }
+    });
+})
